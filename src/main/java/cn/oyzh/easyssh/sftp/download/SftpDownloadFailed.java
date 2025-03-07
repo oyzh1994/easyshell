0f569d67e5bf1b6c06bcbd51759ba89f7746daba
oyzh <oyzh.1994@qq.com>
2025-03-07 23:37:40 +0800

    1. 文件下载失败新增提示

---
+++ src/main/java/cn/oyzh/easyssh/sftp/download/SftpDownloadFailed.java
@@ -0,0 +1,17 @@
+package cn.oyzh.easyssh.sftp.download;
+
+import lombok.Data;
+
+/**
+ * @author oyzh
+ * @since 2025-03-07
+ */
+@Data
+public class SftpDownloadFailed {
+
+    private String remote;
+
+    private int fileCount;
+
+    private String fileName;
+}
