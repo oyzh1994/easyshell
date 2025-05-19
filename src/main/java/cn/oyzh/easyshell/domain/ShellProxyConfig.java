19be5982b602ed11f53872a4631908b5e254c35f
oyzh <oyzh.1994@q.qcom>
2025-05-19 18:37:22 +0800

    1. ssh相关完善

--- src/main/java/cn/oyzh/easyshell/domain/ShellProxyConfig.java
+++ src/main/java/cn/oyzh/easyshell/domain/ShellProxyConfig.java
@@ -1,17 +1,16 @@
 package cn.oyzh.easyshell.domain;
 
+import cn.oyzh.ssh.domain.SSHProxyConfig;
 import cn.oyzh.store.jdbc.Column;
 import cn.oyzh.store.jdbc.PrimaryKey;
 import cn.oyzh.store.jdbc.Table;
-
-import java.io.Serializable;
 
 /**
  * @author oyzh
  * @since 2025-04-14
  */
 @Table("t_proxy")
-public class ShellProxyConfig implements Serializable {
+public class ShellProxyConfig extends SSHProxyConfig {
 
     /**
      * 所属连接id
@@ -25,42 +24,6 @@ public class ShellProxyConfig implements Serializable {
     @Column
     @PrimaryKey
     private String id;
-
-    /**
-     * 代理协议
-     */
-    @Column
-    private String protocol;
-
-    /**
-     * 连接地址
-     */
-    @Column
-    private String host;
-
-    /**
-     * 连接端口
-     */
-    @Column
-    private int port;
-
-    /**
-     * 认证类型
-     */
-    @Column
-    private String authType;
-
-    /**
-     * 用户名
-     */
-    @Column
-    private String user;
-
-    /**
-     * 密码
-     */
-    @Column
-    private String password;
 
     public String getIid() {
         return iid;
@@ -76,69 +39,5 @@ public class ShellProxyConfig implements Serializable {
 
     public void setId(String id) {
         this.id = id;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAuthType() {
-        return authType;
-    }
-
-    public void setAuthType(String authType) {
-        this.authType = authType;
-    }
-
-    public String getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(String protocol) {
-        this.protocol = protocol;
-    }
-
-    public boolean isHttpProxy() {
-        return "http".equalsIgnoreCase(this.protocol);
-    }
-
-    public boolean isSocks4Proxy() {
-        return "socks4".equalsIgnoreCase(this.protocol);
-    }
-
-    public boolean isSocks5Proxy() {
-        return "socks5".equalsIgnoreCase(this.protocol);
-    }
-
-    public boolean isPasswordAuth() {
-        return "password".equalsIgnoreCase(this.authType);
     }
 }
