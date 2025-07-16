e39d720cb4ebb1ca8decb12e3007fe89e0bd774d
oyzh <oyzh.1994@q.qcom>
2025-07-16 16:14:03 +0800

    1. 细节调整

--- src/main/java/cn/oyzh/easyshell/fx/s3/ShellS3BucketTableView.java
+++ src/main/java/cn/oyzh/easyshell/fx/s3/ShellS3BucketTableView.java
@@ -67,8 +67,10 @@ public class ShellS3BucketTableView extends FXTableView<ShellS3Bucket> {
     public void loadBucket() {
         try {
             if (this.client != null) {
-                List<ShellS3Bucket> buckets = this.client.listBuckets();
-                this.setItem(buckets);
+                StageManager.showMask(() -> {
+                    List<ShellS3Bucket> buckets = this.client.listBuckets();
+                    this.setItem(buckets);
+                });
             }
         } catch (Exception ex) {
             ex.printStackTrace();
