ce811563f297828b5c2553c17f599bf1befd85ae
oyzh <oyzh.1994@qq.com>
2025-08-14 09:38:49 +0800

    1. 部分类移动

--- src/main/java/cn/oyzh/easyshell/fx/ShellDataEditorPane.java
+++ src/main/java/cn/oyzh/easyshell/fx/ShellDataEditorPane.java
@@ -2,7 +2,7 @@ package cn.oyzh.easyshell.fx;
 
 import cn.oyzh.easyshell.domain.ShellSetting;
 import cn.oyzh.easyshell.store.ShellSettingStore;
-import cn.oyzh.fx.editor.EditorPane;
+import cn.oyzh.fx.editor.rsyntaxtextarea.EditorPane;
 import cn.oyzh.fx.plus.font.FontManager;
 import javafx.scene.text.Font;
 
--- src/main/java/cn/oyzh/easyshell/fx/ShellJsonEditorPane.java
+++ src/main/java/cn/oyzh/easyshell/fx/ShellJsonEditorPane.java
@@ -2,7 +2,7 @@ package cn.oyzh.easyshell.fx;
 
 import cn.oyzh.easyshell.domain.ShellSetting;
 import cn.oyzh.easyshell.store.ShellSettingStore;
-import cn.oyzh.fx.editor.JsonEditorPane;
+import cn.oyzh.fx.editor.rsyntaxtextarea.JsonEditorPane;
 import cn.oyzh.fx.plus.font.FontManager;
 import javafx.scene.text.Font;
 
