cf4335dfa62ac6e59a77d759a1acb373ac2f29d4
oyzh <oyzh.1994@q.qcom>
2025-09-22 17:04:19 +0800

    1. github-actions脚本测试13

---
+++ package/pack.bat
@@ -0,0 +1,5 @@
+@echo off
+set ARG1=%1
+call mvn package -DskipTests
+rem call mvn test-compile
+call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
