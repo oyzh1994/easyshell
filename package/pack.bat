3dfbd3e7027f259bce8d156d09f6de94a0b6e350
oyzh <oyzh.1994@qq.com>
2025-09-22 19:51:21 +0800

    1. github-actions脚本测试28

--- package/pack.bat
+++ package/pack.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001
 set ARG1=%1
-call mvn package -DskipTests
+call mvn package -DskipTests -Dfile.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8
 rem call mvn test-compile
-call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dexec.jvmArgs="-Dfile.encoding=UTF-8" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
+call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dfile.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8 -Dexec.jvmArgs="-Dfile.encoding=UTF-8" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
