57d848ea1093d06de540ae4cfba59d8deb4c5c07
oyzh <oyzh.1994@qq.com>
2025-10-23 18:26:18 +0800

    1. AppImage格式重启测试

--- src/main/java/cn/oyzh/easyshell/util/ShellProcessUtil.java
+++ src/main/java/cn/oyzh/easyshell/util/ShellProcessUtil.java
@@ -1,9 +1,12 @@
 package cn.oyzh.easyshell.util;
 
+import cn.oyzh.common.log.JulLog;
 import cn.oyzh.common.system.ProcessUtil;
 import cn.oyzh.fx.plus.window.StageManager;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 /**
  * @author oyzh
@@ -16,6 +19,22 @@ public class ShellProcessUtil {
      */
     public static void restartApplication() {
         try {
+            // 运行在appImage格式中
+            if (ProcessUtil.isRunningInAppImage()) {
+                JulLog.info("running in AppImage...");
+                String appImagePath = System.getenv("APPIMAGE");
+                // 工作目录
+                File dir = new File(appImagePath).getParentFile();
+                // 构建重启命令
+                ProcessBuilder  builder = new ProcessBuilder("nohup", appImagePath, "&");
+                Map<String, String> env = builder.environment();
+                env.put("LD_LIBRARY_PATH", "/path/to/appimage/libs:" + env.getOrDefault("LD_LIBRARY_PATH", ""));
+                // 设置运行目录
+                builder.directory(dir);
+                JulLog.info("running in AppImage...");
+                builder.start();
+                return;
+            }
             ProcessUtil.restartApplication2(100, StageManager::exit);
         } catch (IOException ex) {
             ex.printStackTrace();
