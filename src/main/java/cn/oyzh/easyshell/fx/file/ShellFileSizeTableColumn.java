1efc8f649ca0289ebc5142661609b7e3f3c4e852
oyzh <oyzh.1994@q.qcom>
2026-05-27 17:55:17 +0800

    fix: 文件树按文件大小排序不符合预期

---
+++ src/main/java/cn/oyzh/easyshell/fx/file/ShellFileSizeTableColumn.java
@@ -0,0 +1,37 @@
+package cn.oyzh.easyshell.fx.file;
+
+import cn.oyzh.easyshell.file.ShellFile;
+import cn.oyzh.fx.plus.controls.table.FXTableColumn;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
+
+/**
+ *
+ * @author oyzh
+ * @since 2026-05-27
+ */
+public class ShellFileSizeTableColumn extends FXTableColumn<ShellFile, Long> {
+
+    @Override
+    protected Callback<TableColumn<ShellFile, Long>, TableCell<ShellFile, Long>> cellFactory() {
+        return col -> new TableCell<>() {
+            @Override
+            protected void updateItem(Long item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    super.setText(null);
+                } else {
+                    ShellFile file = this.getTableRow().getItem();
+                    super.setText(file.getFileSizeDisplay());
+                }
+            }
+        };
+    }
+
+    @Override
+    public void initNode() {
+        this.setCellFactory(this.cellFactory());
+        super.initNode();
+    }
+}
