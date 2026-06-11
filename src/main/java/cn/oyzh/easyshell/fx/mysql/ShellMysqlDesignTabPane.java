cecfa344c2a1c1f4c30c800dd92c990c8810ccb8
oyzh <oyzh.1994@q.qcom>
2026-06-11 11:18:47 +0800

    update: 细节调整

--- src/main/java/cn/oyzh/easyshell/fx/mysql/ShellMysqlDesignTabPane.java
+++ src/main/java/cn/oyzh/easyshell/fx/mysql/ShellMysqlDesignTabPane.java
@@ -1,18 +1,18 @@
-package cn.oyzh.easyshell.fx.mysql;
-
-import cn.oyzh.fx.plus.controls.tab.FXTabPane;
-
-/**
- *
- * @author oyzh
- * @since 2025-10-31
- */
-public class ShellMysqlDesignTabPane extends FXTabPane {
-
-    @Override
-    public void initNode() {
-        super.initNode();
-        // this.setupSelectCountListener();
-        this.setupRefreshListener();
-    }
-}
+//package cn.oyzh.easyshell.fx.mysql;
+//
+//import cn.oyzh.fx.plus.controls.tab.FXTabPane;
+//
+///**
+// *
+// * @author oyzh
+// * @since 2025-10-31
+// */
+//public class ShellMysqlDesignTabPane extends FXTabPane {
+//
+//    @Override
+//    public void initNode() {
+//        super.initNode();
+//        // this.setupSelectCountListener();
+//        this.setupRefreshListener();
+//    }
+//}
