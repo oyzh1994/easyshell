2d65d8e71a98616f565c99e37a2d72e01883a7be
oyzh <oyzh.1994@qq.com>
2026-08-20 11:00:00 +0800

    fix: 部分组件ui显示不符合预期

--- src/main/java/cn/oyzh/easyshell/fx/ssh/ShellGpuEditor.java
+++ src/main/java/cn/oyzh/easyshell/fx/ssh/ShellGpuEditor.java
@@ -3,6 +3,7 @@ package cn.oyzh.easyshell.fx.ssh;
 import cn.oyzh.easyshell.domain.ShellSetting;
 import cn.oyzh.easyshell.store.ShellSettingStore;
 import cn.oyzh.fx.editor.incubator.Editor;
+import cn.oyzh.fx.plus.domain.AppSetting;
 import cn.oyzh.fx.plus.font.FontUtil;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -17,7 +18,7 @@ public class ShellGpuEditor extends Editor {
     protected Font getEditorFont() {
         ShellSetting setting = ShellSettingStore.SETTING;
         FontWeight weight = FontWeight.findByWeight(setting.getFontWeight());
-        return FontUtil.newFont("Monaco", weight, setting.getFontSize());
+        return FontUtil.newFont(AppSetting.defaultTerminalFontFamily(), weight, setting.getFontSize());
     }
 
     @Override
