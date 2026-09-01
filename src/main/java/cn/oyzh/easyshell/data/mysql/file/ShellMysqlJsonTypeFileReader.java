3d9c163c1380ba67a9d28d991cec4df27072ebda
oyzh <oyzh.1994@qq.com>
2026-09-01 18:28:39 +0800

    update: fastjson升级为标准版fastjson2

--- src/main/java/cn/oyzh/easyshell/data/mysql/file/ShellMysqlJsonTypeFileReader.java
+++ src/main/java/cn/oyzh/easyshell/data/mysql/file/ShellMysqlJsonTypeFileReader.java
@@ -2,13 +2,12 @@ package cn.oyzh.easyshell.data.mysql.file;
 
 import cn.oyzh.common.file.FileUtil;
 import cn.oyzh.easyshell.data.mysql.config.ShellMysqlDataImportConfig;
-import com.alibaba.fastjson.JSONReader;
+import com.alibaba.fastjson2.JSONReader;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -29,43 +28,47 @@ public class ShellMysqlJsonTypeFileReader extends ShellMysqlTypeFileReader {
 
     public ShellMysqlJsonTypeFileReader(File file, ShellMysqlDataImportConfig config) throws FileNotFoundException {
         this.config = config;
-        this.reader = new JSONReader(FileUtil.getReader(file, Charset.forName(config.getCharset())));
+        this.reader = JSONReader.of(FileUtil.getReader(file, Charset.forName(config.getCharset())));
         this.init();
     }
 
     @Override
     protected void init() {
         // 初始化
-        if (this.reader.hasNext()) {
-            if (this.config.getRecordLabel() == null) {
+        if (this.reader.isEnd()) {
+            return;
+        }
+        if (this.config.getRecordLabel() == null) {
+            // 纯数组格式: [...]
+            this.reader.startArray();
+        } else {
+            // 对象包装格式: {"recordLabel": [...]}
+            this.reader.nextIfObjectStart();
+            String key = this.reader.readFieldName();
+            if (key != null && key.equalsIgnoreCase(this.config.getRecordLabel())) {
                 this.reader.startArray();
-            } else {
-                this.reader.startObject();
-                String key = this.reader.readString();
-                if (key.equalsIgnoreCase(this.config.getRecordLabel())) {
-                    this.reader.startArray();
-                }
             }
         }
     }
 
     @Override
     public Map<String, Object> readObject() {
-        if (this.reader.hasNext()) {
-            return this.reader.readObject(HashMap.class);
+        // 到达流末尾或数组末尾（]）时返回 null
+        if (this.reader.isEnd() || this.reader.nextIfMatch(']')) {
+            return null;
         }
-        return null;
+        Map<String, Object> object = this.reader.readObject();
+        // 消费元素间的逗号分隔符
+        this.reader.nextIfComma();
+        return object;
     }
 
     @Override
     public void close() throws IOException {
         if (this.reader != null) {
-            if (this.config.getRecordLabel() == null) {
-                this.reader.endArray();
-                // this.reader.endArray();
-            } else {
-                this.reader.endArray();
-                this.reader.endObject();
+            if (this.config.getRecordLabel() != null) {
+                // 消费包装对象的结束符 '}'
+                this.reader.nextIfObjectEnd();
             }
             this.reader.close();
             this.reader = null;
