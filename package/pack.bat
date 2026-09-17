94088b9acaa51aec7005c9e65541b61b2d283feb
oyzh <oyzh.1994@q.qcom>
2026-09-17 08:37:04 +0800

    fix: github actions的windows主机输出乱码

--- package/pack.bat
+++ package/pack.bat
@@ -1,6 +1,7 @@
 @echo off
-chcp 65001
+chcp 65001 >nul
 set ARG1=%1
-call mvn package -DskipTests -Dfile.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8
+call mvn package -DskipTests -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8
 rem call mvn test-compile
-call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dfile.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8 -Dexec.jvmArgs="-Dfile.encoding=UTF-8" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
+call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8 -Dexec.jvmArgs="-Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
