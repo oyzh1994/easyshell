14fbfa38fe546af605bf2f7209af3fbade3551de
oyzh <oyzh.1994@q.qcom>
2026-09-20 00:05:20 +0800

    update: win arm打包测试3

---
+++ package/pack_woa.bat
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001 >nul
+set ARG1=%1
+call mvn package -Djavafx.platform=win -DskipTests
+rem call mvn test-compile
+call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
