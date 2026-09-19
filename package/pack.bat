ee23dc81b6cd0034ab9b2f272f5c76e3a122c382
oyzh <oyzh.1994@q.qcom>
2026-09-19 13:08:32 +0800

    update: win arm打包准备

--- package/pack.bat
+++ package/pack.bat
@@ -1,7 +1,6 @@
 @echo off
 chcp 65001 >nul
 set ARG1=%1
-set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8
-call mvn package -DskipTests -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8
+call mvn package -DskipTests
 rem call mvn test-compile
-call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8 -Dproject.build.sourceEncoding=UTF-8 -Dexec.jvmArgs="-Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 -Dstderr.encoding=UTF-8" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
+call mvn exec:java -Dexec.mainClass="cn.oyzh.easyshell.test.Pack" -Dexec.args="%ARG1%" -Dexec.testClasspathScope=test -X
